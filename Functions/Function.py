5bc8d9798d97b5d9e595bd29f5a5e0cdc57298c5
sayhamza <sayedhamza88882651@gmail.com>
2023-12-22 02:52:47 +0530

    done

--- Functions/Function.py
+++ Functions/Function.py
@@ -9,16 +9,20 @@
 # In Python a function is defined using the def keyword:
 
 # ExampleGet your own Python Server
-# def my_function():
-#   print("Hello from a function")
+
+def my_function():
+  print("Hello from a function")
+
 # Calling a Function
 # To call a function, use the function name followed by parenthesis:
 
 # Example
-# def my_function():
-#   print("Hello from a function")
 
-# my_function()
+def my_function():
+  print("Hello from a function")
+
+my_function()
+
 # Arguments
 # Information can be passed into functions as arguments.
 
@@ -27,12 +31,14 @@
 # The following example has a function with one argument (fname). When the function is called, we pass along a first name, which is used inside the function to print the full name:
 
 # Example
-# def my_function(fname):
-#   print(fname + " Refsnes")
 
-# my_function("Emil")
-# my_function("Tobias")
-# my_function("Linus")
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+
 # Arguments are often shortened to args in Python documentations.
 
 # Parameters or Arguments?
@@ -50,18 +56,21 @@
 # Example
 # This function expects 2 arguments, and gets 2 arguments:
 
-# def my_function(fname, lname):
-#   print(fname + " " + lname)
+def my_function(fname, lname):
+  print(fname + " " + lname)
 
-# my_function("Emil", "Refsnes")
+my_function("Emil", "Refsnes")
+
 # If you try to call the function with 1 or 3 arguments, you will get an error:
 # Example
 # This function expects 2 arguments, but gets only 1:
 
-# def my_function(fname, lname):
-#   print(fname + " " + lname)
+def my_function(fname, lname):
+  print(fname + " " + lname)
 
-# my_function("Emil")
+my_function("Emil")
+
+
 # Arbitrary Arguments, *args
 # If you do not know how many arguments that will be passed into your function, add a * before the parameter name in the function definition.
 
@@ -70,10 +79,11 @@
 # Example
 # If the number of arguments is unknown, add a * before the parameter name:
 
-# def my_function(*kids):
-#   print("The youngest child is " + kids[2])
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
 
-# my_function("Emil", "Tobias", "Linus")
+my_function("Emil", "Tobias", "Linus")
+
 # Arbitrary Arguments are often shortened to *args in Python documentations.
 
 # Keyword Arguments
@@ -81,11 +91,14 @@
 
 # This way the order of the arguments does not matter.
 
-# Example
-# def my_function(child3, child2, child1):
-#   print("The youngest child is " + child3)
 
-# my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+# Example
+
+def my_function(child3, child2, child1):
+  print("The youngest child is " + child3)
+
+my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+
 # The phrase Keyword Arguments are often shortened to kwargs in Python documentations.
 
 # Arbitrary Keyword Arguments, **kwargs
@@ -96,10 +109,11 @@
 # Example
 # If the number of keyword arguments is unknown, add a double ** before the parameter name:
 
-# def my_function(**kid):
-#   print("His last name is " + kid["lname"])
+def my_function(**kid):
+  print("His last name is " + kid["lname"])
 
-# my_function(fname = "Tobias", lname = "Refsnes")
+my_function(fname = "Tobias", lname = "Refsnes")
+
 # Arbitrary Kword Arguments are often shortened to **kwargs in Python documentations.
 
 # Default Parameter Value
@@ -108,42 +122,52 @@
 # If we call the function without argument, it uses the default value:
 
 # Example
-# def my_function(country = "Norway"):
-#   print("I am from " + country)
 
-# my_function("Sweden")
-# my_function("India")
-# my_function()
-# my_function("Brazil")
+def my_function(country = "Norway"):
+  print("I am from " + country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
+
 # Passing a List as an Argument
 # You can send any data types of argument to a function (string, number, list, dictionary etc.), and it will be treated as the same data type inside the function.
 
 # E.g. if you send a List as an argument, it will still be a List when it reaches the function:
 
 # Example
-# def my_function(food):
-#   for x in food:
-#     print(x)
 
-# fruits = ["apple", "banana", "cherry"]
+def my_function(food):
+  for x in food:
+    print(x)
 
-# my_function(fruits)
+fruits = ["apple", "banana", "cherry"]
+
+my_function(fruits)
+
+
 # Return Values
 # To let a function return a value, use the return statement:
 
 # Example
-# def my_function(x):
-#   return 5 * x
 
-# print(my_function(3))
-# print(my_function(5))
-# print(my_function(9))
+def my_function(x):
+  return 5 * x
+
+print(my_function(3))
+print(my_function(5))
+print(my_function(9))
+
 # The pass Statement
 # function definitions cannot be empty, but if you for some reason have a function definition with no content, put in the pass statement to avoid getting an error.
 
 # Example
-# def myfunction():
-#   pass
+
+def myfunction():
+  pass
+
+
 # Recursion
 # Python also accepts function recursion, which means a defined function can call itself.
 
@@ -158,13 +182,14 @@
 # Example
 # Recursion Example
 
-# def tri_recursion(k):
-#   if(k > 0):
-#     result = k + tri_recursion(k - 1)
-#     print(result)
-#   else:
-#     result = 0
-#   return result
 
-# print("\n\nRecursion Example Results")
-# tri_recursion(6)
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("\n\nRecursion Example Results")
+tri_recursion(6)
